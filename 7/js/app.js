e477becf69b2dc1f718425ff375426fdf897f10f
Keks <keks@htmlacademy.ru>
2022-09-18 03:58:45 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/app.js
@@ -0,0 +1,7 @@
+document.getElementById('header-toggle').addEventListener('click', function () {
+  document.getElementById('nav-block').classList.toggle('nav-block--close')
+})
+
+document.getElementById('header-toggle').addEventListener('click', function () {
+  document.getElementById('header-toggle').classList.toggle('main-header__toggle--close')
+})
