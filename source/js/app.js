8a36b3489390b04498aa8829a00cd940e2f5c587
Никита <1nik2121@gmail.com>
2022-09-25 15:40:36 +0300

    Правки

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,21 @@
+  let header = document.getElementById('main-header');
+  let logo = document.getElementById('logo-header');
+  let nav = document.getElementById('nav-block');
+  let toggler = document.getElementById('header-toggle')
+
+  header.classList.remove('main-header--static');
+  logo.classList.remove('main-header__logo--static');
+  nav.classList.remove('nav-block--static');
+  nav.classList.add('nav-block--close');
+  toggler.classList.remove('main-header__toggle--static');
+
+
+function indexIntro (){
+  let indexIntro = document.getElementById('index-intro');
+  indexIntro.classList.remove('index-intro--static');
+}
+
+
 document.getElementById('header-toggle').addEventListener('click', function () {
   document.getElementById('nav-block').classList.toggle('nav-block--close')
 })
